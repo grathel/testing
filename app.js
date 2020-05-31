13d03082010c608ff629468e6c73aac767faf4d2
grathel <truszkowski.mat@gmail.com>
2020-05-31 17:33:23 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
-const http = require("http");
-
-http.createServer(function (request, response) {
-    // Send the HTTP header 
-    // HTTP Status: 200 : OK
-    // Content Type: text/plain
-    response.writeHead(200, { 'Content-Type': 'text/plain' });
-
-    // Send the response body as "Hello World"
-    response.end('Hello World\n');
-}).listen(5000);
-
-// Console will print the message
-console.log('Server running at http://127.0.0.1:5000/');
+// using the http module
+import { createServer } from 'http';
+let
+    // look for PORT environment variable, 
+    // else look for CLI argument,
+    // else use hard coded value for port 8080
+    port = process.env.PORT || process.argv[2] || 8080;
+ 
+// create a simple server
+let server = createServer(function (req, res) {
+ 
+        res.writeHead(200, {
+            'Content-Type': 'text/plain'
+        });
+        res.write('hello heroku!', 'utf-8');
+        res.end();
+ 
+    });
+ 
+// listen on the port
+server.listen(port, function () {
+ 
+    console.log('app up on port: ' + port);
+ 
+});
