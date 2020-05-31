bcaabc3b2d37258d37d20727ebc7cd488958e44c
grathel <truszkowski.mat@gmail.com>
2020-05-31 17:38:13 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,25 +1,17 @@
-// using the http module
-let http = require('http'),
- 
-// look for PORT environment variable, 
-// else look for CLI argument,
-// else use hard coded value for port 8080
-port = process.env.PORT || process.argv[2] || 8080;
- 
-// create a simple server
-let server = http.createServer(function (req, res) {
- 
-        res.writeHead(200, {
-            'Content-Type': 'text/plain'
-        });
-        res.write('hello heroku!', 'utf-8');
-        res.end();
- 
-    });
- 
-// listen on the port
-server.listen(port, function () {
- 
-    console.log('app up on port: ' + port);
- 
+const express = require('express');
+const bodyParser = require('body-parser');
+const pino = require('express-pino-logger')();
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(pino);
+
+app.get('/api/greeting', (req, res) => {
+  const name = req.query.name || 'World';
+  res.setHeader('Content-Type', 'application/json');
+  res.send(JSON.stringify({ greeting: `Hello ${name}!` }));
 });
+
+app.listen(3001, () =>
+  console.log('Express server is running on localhost:3001')
+);
