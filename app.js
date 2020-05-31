652715c051b2daceed69695c9821e7c74b0f6afa
grathel <truszkowski.mat@gmail.com>
2020-05-31 18:11:16 +0200

    test

--- app.js
+++ app.js
@@ -7,9 +7,9 @@ app.use(express.static("public"))
 
 // define the first route
 app.get("/", function (req, res) {
-  res.send("<h1>Hello World!</h1>")
+    res.send("<h1>Hello World!</h1>")
 })
 
 // start the server listening for requests
-app.listen(process.env.PORT || 3000, 
-	() => console.log("Server is running..."));
+app.listen(process.env.PORT || 3000,
+    () => console.log("Server is running..."));
